c093e879c43f405c83bf21debf8b3c23b93a1645
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-10-10 00:53:43 +0500

    install axios and start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+    <div className="App">
+      <p className="search">Search...</p>
+      
+    </div>
+    </>
+  )
+}
---
+++ src/components/searchComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function searchComponents() {
+  return (
+    <>
+    <div className=""></div>
+    </>
+  )
+}
